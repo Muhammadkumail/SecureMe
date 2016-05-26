1700851b3e30bd340e7967af467a02e5e294b835
Muhammad kumail <muhammad.kumail449@gmail.com>
2016-05-26 22:51:11 +0500

    mobile to mobile notification added

--- app/src/main/java/com/secureme/secureme/HomeActivity.java
+++ app/src/main/java/com/secureme/secureme/HomeActivity.java
@@ -1,17 +1,37 @@
 package com.secureme.secureme;
 
-import android.content.Intent;
-import android.net.Uri;
+import android.app.ProgressDialog;
+import android.content.Context;
+
+import android.net.ConnectivityManager;
+
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.ImageView;
+
+import android.widget.Toast;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class HomeActivity extends AppCompatActivity {
 
     Boolean flag = false;
-
+    String mNotificationNumber = "";
 
 
     @Override
@@ -19,52 +39,114 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+    }
+
+    private boolean isNetworkConnected() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        return cm.getActiveNetworkInfo() != null;
+    }
 
 
+
+    public void onClick_cameraSendNotification(View view) {
+        mNotificationNumber = "camera";
+        if (isNetworkConnected()) {
+            new asyncTask_sendNotification().execute();
+        } else {
+            errorMessage("Please Check Internet connection");
+        }
+
+    }
+    public void onClick_gpsSendNotification(View view) {
+        mNotificationNumber = "gps";
+        if (isNetworkConnected()) {
+            new asyncTask_sendNotification().execute();
+        } else {
+            errorMessage("Please Check Internet connection");
+        }
+
+    }
+    public void onClick_alaramSendNotification(View view) {
+        mNotificationNumber = "alaram";
+        if (isNetworkConnected()) {
+            new asyncTask_sendNotification().execute();
+        } else {
+            errorMessage("Please Check Internet connection");
+        }
+
+    }
+    public void onClick_switchSendNotification(View view) {
+        mNotificationNumber = "switch";
+        if (isNetworkConnected()) {
+            new asyncTask_sendNotification().execute();
+        } else {
+            errorMessage("Please Check Internet connection");
+        }
 
     }
 
-  /*  public void onClickLock(View view) {
-        ImageView lockImageView = (ImageView) findViewById(R.id.home_key_lock);
+    public void errorMessage(String Errormessage) {
+        Toast.makeText(HomeActivity.this, Errormessage, Toast.LENGTH_SHORT).show();
 
-        if (flag == false) {
-            lockImageView.setImageResource(R.drawable.ic_key_unlock);
-            flag = true;
-        } else {
+    }
 
-            lockImageView.setImageResource(R.drawable.ic_key_lock);
-            flag = false;
-        }
-    }*/
+    private class asyncTask_sendNotification extends AsyncTask<Void, Void, String> {
+        ProgressDialog progressDialog;
 
-    /*public void onClickSiren(View view) {
-        ImageView lockImageView = (ImageView) findViewById(R.id.home_siren);
+        @Override
+        protected void onPreExecute() {
+            progressDialog = ProgressDialog.show(HomeActivity.this, "Sending...", "wait", true);
 
-        if (flag == false) {
-            lockImageView.setImageResource(R.drawable.ic_siren);
-            flag = true;
-        } else {
-
-            lockImageView.setImageResource(R.drawable.ic_mute);
-            flag = false;
-        }
-    }*/
-
-   /* public void onClick_Google_map(View view) {
-        Intent sintent = new Intent();
-        sintent.setClass(HomeActivity.this, GoogleMapActivity.class);
-        startActivity(sintent);
-
-    }*/
-
-   /* public void onClick_PhoneCall(View view) {
-        Intent intent = new Intent(Intent.ACTION_DIAL);
-        intent.setData(Uri.parse("tel:" + "+923362175378"));
-        if (intent.resolveActivity(getPackageManager()) != null) {
-            startActivity(intent);
+            super.onPreExecute();
         }
 
+        @Override
+        protected String doInBackground(Void... params) {
+            String msg = "";
+            try {
+                if (mNotificationNumber.length() > -0) {
 
-    }*/
+
+                    msg = sendNotificationToWebServer(mNotificationNumber);
+
+                }
+            } catch (Exception ex) {
+                msg = "Error :" + ex.getMessage();
+            }
+            return msg;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            progressDialog.dismiss();
+            mNotificationNumber = "";
+
+            errorMessage("Your Message Has been sent");
+
+        }
+    }
+
+    public String sendNotificationToWebServer(String mNotificationNumber) {
+        String url = "http://friendsfashion.net/android/secureme/securemeNotification.php";
+        String strResponse = "No response";
+        HttpClient httpclient = new DefaultHttpClient();
+        HttpPost httppost = new HttpPost(url);
+        try {
+            List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(1);
+            nameValuePairs.add(new BasicNameValuePair("notificationNumber", mNotificationNumber));
+            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+            HttpResponse response = httpclient.execute(httppost);
+            strResponse = EntityUtils.toString(response.getEntity());
+        } catch (ClientProtocolException e) {
+            strResponse = e.getMessage();
+        } catch (IOException e) {
+            Log.e("IOException:", e.getMessage());
+            strResponse = e.getMessage();
+        }
+        return strResponse;
+    }
+
 
 }
