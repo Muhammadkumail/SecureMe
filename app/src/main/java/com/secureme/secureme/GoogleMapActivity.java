a29522a3d3ce822919ab0ad4c1cf4c7f8e1e8492
Muhammad kumail <muhammad.kumail449@gmail.com>
2016-06-10 18:07:00 +0500

    gps location find added

--- app/src/main/java/com/secureme/secureme/GoogleMapActivity.java
+++ app/src/main/java/com/secureme/secureme/GoogleMapActivity.java
@@ -1,7 +1,9 @@
 package com.secureme.secureme;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
@@ -17,28 +19,71 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class GoogleMapActivity extends AppCompatActivity {
 
     GoogleMap map;
+    float lat, lng;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_google_map);
 
+        Intent recievedIntent = getIntent();
+
+        lat = (float) recievedIntent.getSerializableExtra("lat");
+        lng = (float)  recievedIntent.getSerializableExtra("lng");
         get_location();
 
     }
 
-    private void get_location() {
-        float lat, lng;
 
+    private void get_location() {
+        /*float lat, lng;
 
         lat = (float) 24.9324;
-        lng = (float) 67.1127;
+        lng = (float) 67.1127;*/
+
+
+        try {
+            map = ((MapFragment) getFragmentManager().findFragmentById(R.id.google_map_fragment)).getMap();
+            MarkerOptions k = new MarkerOptions().position(new LatLng(lat, lng)).title("Ned University").icon(BitmapDescriptorFactory.fromResource(R.drawable.marker)).draggable(true).snippet("");
+            map.addMarker(k);
+            map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            map.setMyLocationEnabled(true);
+            map.getUiSettings().setZoomControlsEnabled(true);
+            map.getUiSettings().setAllGesturesEnabled(true);
+            map.getUiSettings().setCompassEnabled(true);
+            map.getUiSettings().setMyLocationButtonEnabled(true);
+            map.getUiSettings().setRotateGesturesEnabled(true);
+            map.getUiSettings().setTiltGesturesEnabled(true);
+
+            CameraPosition cameraPosition = new CameraPosition.Builder().target(new LatLng(lat, lng)).zoom(12).build();
+                        map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+        } catch (Exception e) {
+                     e.printStackTrace();
+                  }
+    }
+
+   /* private void get_location() {
+
 
         try {
             map = ((MapFragment) getFragmentManager().findFragmentById(R.id.google_map_fragment)).getMap();
             MarkerOptions k = new MarkerOptions().position(new LatLng(lat, lng)).title("Ned University").icon(BitmapDescriptorFactory.fromResource(R.drawable.gps)).draggable(true).snippet("");
             map.addMarker(k);
             map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
             map.setMyLocationEnabled(true);
             map.getUiSettings().setZoomControlsEnabled(true);
             map.getUiSettings().setAllGesturesEnabled(true);
@@ -53,14 +98,14 @@ public class GoogleMapActivity extends AppCompatActivity {
             e.printStackTrace();
         }
     }
+*/
 
-
-    public void onClick_Google_map(View view) {
+   /* public void onClick_Google_map(View view) {
 
         Toast.makeText(GoogleMapActivity.this, "Searching...", Toast.LENGTH_LONG).show();
         get_location();
 
-    }
+    }*/
 
 
-}
+        }
--- app/src/main/java/com/secureme/secureme/HomeActivity.java
+++ app/src/main/java/com/secureme/secureme/HomeActivity.java
@@ -3,6 +3,7 @@ package com.secureme.secureme;
 import android.app.ProgressDialog;
 import android.content.Context;
 
+import android.content.Intent;
 import android.net.ConnectivityManager;
 
 import android.os.AsyncTask;
@@ -31,16 +32,27 @@ import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
 
-    Boolean flag = false;
     String mNotificationNumber = "";
+    String mNotificationType = "";
+
     private boolean isBulbOn = true;
-    ImageView iv;
+    private boolean isalarmOn = true;
+    private boolean isdoorOpen = true;
+    ImageView ivalarm;
+    ImageView ivdoor;
+    ImageView ivswith;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        ivalarm = (ImageView) findViewById(R.id.home_imageView_alarm);
+        ivdoor = (ImageView) findViewById(R.id.home_imageView_door);
+        ivswith = (ImageView) findViewById(R.id.home_imageView_switch);
+
+        new asyncTask_getStatus().execute();
+
 
     }
 
@@ -51,64 +63,78 @@ public class HomeActivity extends AppCompatActivity {
     }
 
 
-    public void onClick_cameraSendNotification(View view) {
-        mNotificationNumber = "camera";
-        if (isNetworkConnected()) {
-            new asyncTask_sendNotification().execute();
+    public void onClick_doorSendNotification(View view) {
+
+
+        if (isdoorOpen) {
+            ivdoor.setImageResource((R.drawable.ic_lock));
+
+
+            if (isNetworkConnected()) {
+                mNotificationNumber = "1";
+                mNotificationType = "door";
+                new asyncTask_sendNotification().execute();
+            } else {
+                errorMessage("Please Check Internet connection");
+            }
+
+            isdoorOpen = false;
         } else {
-            errorMessage("Please Check Internet connection");
+            ivdoor.setImageResource((R.drawable.ic_unlock));
+
+            if (isNetworkConnected()) {
+                mNotificationNumber = "2";
+                mNotificationType = "door";
+                new asyncTask_sendNotification().execute();
+            } else {
+                errorMessage("Please Check Internet connection");
+            }
+            isdoorOpen = true;
         }
 
     }
 
-    public void onClick_gpsSendNotification(View view) {
-        mNotificationNumber = "gps";
-        if (isNetworkConnected()) {
-            new asyncTask_sendNotification().execute();
-        } else {
-            errorMessage("Please Check Internet connection");
-        }
-
-    }
 
     public void onClick_alaramSendNotification(View view) {
-        iv = (ImageView) findViewById(R.id.home_imageView_alarm);
 
-        if (isBulbOn) {
-            iv.setImageResource((R.drawable.ic_alarm_on));
+
+        if (isalarmOn) {
+            ivalarm.setImageResource((R.drawable.ic_alarm_on));
 
 
             if (isNetworkConnected()) {
                 mNotificationNumber = "3";
+                mNotificationType = "alarm";
                 new asyncTask_sendNotification().execute();
             } else {
                 errorMessage("Please Check Internet connection");
             }
 
-            isBulbOn = false;
+            isalarmOn = false;
         } else {
-            iv.setImageResource((R.drawable.ic_alarm_off));
+            ivalarm.setImageResource((R.drawable.ic_alarm_off));
 
             if (isNetworkConnected()) {
                 mNotificationNumber = "4";
+                mNotificationType = "alarm";
                 new asyncTask_sendNotification().execute();
             } else {
                 errorMessage("Please Check Internet connection");
             }
-            isBulbOn = true;
+            isalarmOn = true;
         }
     }
 
     public void onClick_switchSendNotification(View view) {
 
-        iv = (ImageView) findViewById(R.id.home_imageView_switch);
 
         if (isBulbOn) {
-            iv.setImageResource((R.drawable.ic_bulb_on));
+            ivswith.setImageResource((R.drawable.ic_bulb_on));
 
 
             if (isNetworkConnected()) {
-                mNotificationNumber = "1";
+                mNotificationNumber = "5";
+                mNotificationType = "switch";
                 new asyncTask_sendNotification().execute();
             } else {
                 errorMessage("Please Check Internet connection");
@@ -116,10 +142,11 @@ public class HomeActivity extends AppCompatActivity {
 
             isBulbOn = false;
         } else {
-            iv.setImageResource((R.drawable.ic_bulb_off));
+            ivswith.setImageResource((R.drawable.ic_bulb_off));
 
             if (isNetworkConnected()) {
-                mNotificationNumber = "2";
+                mNotificationNumber = "6";
+                mNotificationType = "switch";
                 new asyncTask_sendNotification().execute();
             } else {
                 errorMessage("Please Check Internet connection");
@@ -130,10 +157,23 @@ public class HomeActivity extends AppCompatActivity {
 
     }
 
-    public void errorMessage(String Errormessage) {
-        Toast.makeText(HomeActivity.this, Errormessage, Toast.LENGTH_SHORT).show();
+    public void onClick_gpsSendNotification(View view) {
+        mNotificationNumber = "7";
+        mNotificationType = "gps";
+        if (isNetworkConnected()) {
+            new asyncTask_sendNotification().execute();
+            new asyncTask_getgps().execute();
+        } else {
+            errorMessage("Please Check Internet connection");
+        }
 
     }
+
+    public void errorMessage(String Errormessage) {
+        Toast.makeText(HomeActivity.this, Errormessage, Toast.LENGTH_LONG).show();
+
+    }
+
 
     private class asyncTask_sendNotification extends AsyncTask<Void, Void, String> {
         ProgressDialog progressDialog;
@@ -152,7 +192,7 @@ public class HomeActivity extends AppCompatActivity {
                 if (mNotificationNumber.length() > -0) {
 
 
-                    msg = sendNotificationToWebServer(mNotificationNumber);
+                    msg = sendNotificationToWebServer(mNotificationNumber, mNotificationType);
 
                 }
             } catch (Exception ex) {
@@ -167,19 +207,195 @@ public class HomeActivity extends AppCompatActivity {
             progressDialog.dismiss();
             mNotificationNumber = "";
 
-            errorMessage(s);
+            //errorMessage(s);
 
         }
     }
 
-    public String sendNotificationToWebServer(String mNotificationNumber) {
+    public String sendNotificationToWebServer(String mNotificationNumber, String mNotificationType) {
         String url = "http://friendsfashion.net/android/secureme/securemeNotification.php";
         String strResponse = "No response";
         HttpClient httpclient = new DefaultHttpClient();
         HttpPost httppost = new HttpPost(url);
         try {
-            List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(1);
+            List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
             nameValuePairs.add(new BasicNameValuePair("notificationNumber", mNotificationNumber));
+            nameValuePairs.add(new BasicNameValuePair("notificationType", mNotificationType));
+            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+            HttpResponse response = httpclient.execute(httppost);
+            strResponse = EntityUtils.toString(response.getEntity());
+        } catch (ClientProtocolException e) {
+            strResponse = e.getMessage();
+        } catch (IOException e) {
+            Log.e("IOException:", e.getMessage());
+            strResponse = e.getMessage();
+        }
+        return strResponse;
+    }
+
+    private class asyncTask_getStatus extends AsyncTask<Void, Void, String> {
+        ProgressDialog progressDialog;
+
+        @Override
+        protected void onPreExecute() {
+            progressDialog = ProgressDialog.show(HomeActivity.this, "Loading...", "wait", true);
+
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(Void... params) {
+            String msg = "";
+            try {
+
+
+                msg = getStatusFromToWebServer();
+
+
+            } catch (Exception ex) {
+                msg = "Error :" + ex.getMessage();
+            }
+            return msg;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            progressDialog.dismiss();
+            String[] splitResult = result.split(",");
+
+            String swith = splitResult[1];
+            String door = splitResult[3];
+            String alarm = splitResult[5];
+
+            int intswith = Integer.parseInt(swith);
+            int intdoor = Integer.parseInt(door);
+            int intalarm = Integer.parseInt(alarm);
+            try {
+                if (intdoor == 1) {
+                    ivdoor.setImageResource((R.drawable.ic_lock));
+                    isdoorOpen = false;
+
+                }
+                if (intdoor == 2) {
+                    ivdoor.setImageResource((R.drawable.ic_unlock));
+                    isdoorOpen = true;
+
+                }
+
+                if (intalarm == 3) {
+
+                    ivalarm.setImageResource((R.drawable.ic_alarm_on));
+                    isalarmOn = false;
+
+                }
+                if (intalarm == 4) {
+
+                    ivalarm.setImageResource((R.drawable.ic_alarm_off));
+                    isalarmOn = true;
+
+                }
+                if (intswith == 9) {
+                    ivswith.setImageResource((R.drawable.ic_bulb_on));
+                    isBulbOn = false;
+
+                }
+                if (intswith == 10) {
+                    ivswith.setImageResource((R.drawable.ic_bulb_off));
+                    isBulbOn = true;
+
+                }
+            } catch (Exception e) {
+                errorMessage(e.toString());
+                e.printStackTrace();
+            }
+
+
+
+
+
+           /* errorMessage(swith+door+alarm);
+            errorMessage(result);*/
+
+        }
+    }
+
+    public String getStatusFromToWebServer() {
+        String url = "http://friendsfashion.net/android/secureme/getStatus.php";
+        String strResponse = "No response";
+        HttpClient httpclient = new DefaultHttpClient();
+        HttpPost httppost = new HttpPost(url);
+        try {
+            List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+            nameValuePairs.add(new BasicNameValuePair("getStatus", "getStatus"));
+            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+            HttpResponse response = httpclient.execute(httppost);
+            strResponse = EntityUtils.toString(response.getEntity());
+        } catch (ClientProtocolException e) {
+            strResponse = e.getMessage();
+        } catch (IOException e) {
+            Log.e("IOException:", e.getMessage());
+            strResponse = e.getMessage();
+        }
+        return strResponse;
+    }
+
+    private class asyncTask_getgps extends AsyncTask<Void, Void, String> {
+        ProgressDialog progressDialog;
+
+        @Override
+        protected void onPreExecute() {
+            progressDialog = ProgressDialog.show(HomeActivity.this, "Loading...", "wait", true);
+
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(Void... params) {
+            String msg = "";
+            try {
+
+
+                msg = getGpsFromToWebServer();
+
+
+            } catch (Exception ex) {
+                msg = "Error :" + ex.getMessage();
+            }
+            return msg;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            progressDialog.dismiss();
+            String[] splitResult = result.split(",");
+
+            String lan = splitResult[0];
+            String lng = splitResult[1];
+
+
+            float floatLan = Float.parseFloat(lan);
+            float floatlng = Float.parseFloat(lng);
+            Intent intent=new Intent(HomeActivity.this,GoogleMapActivity.class);
+            intent.putExtra("lat",floatLan);
+            intent.putExtra("lng",floatlng);
+            startActivity(intent);
+
+           errorMessage(lan+lng);
+
+
+        }
+    }
+
+    public String getGpsFromToWebServer() {
+        String url = "http://friendsfashion.net/android/secureme/getGps.php";
+        String strResponse = "No response";
+        HttpClient httpclient = new DefaultHttpClient();
+        HttpPost httppost = new HttpPost(url);
+        try {
+            List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+            nameValuePairs.add(new BasicNameValuePair("getGps", "getGps"));
             httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
             HttpResponse response = httpclient.execute(httppost);
             strResponse = EntityUtils.toString(response.getEntity());
